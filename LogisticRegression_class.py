20ae2f5531e76f48e4d48012bc9f99b0531ef788
wey1998 <yitwey1998@hotmail.com>
2023-03-20 21:49:04 +0000

    Move prepare_image_data function into separate file to allow modularity

--- LogisticRegression_class.py
+++ LogisticRegression_class.py
@@ -3,6 +3,7 @@ import os
 from PIL import Image
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+from prepare_image_data import prepare_image_data
 
 class LogisticRegression:
     
@@ -92,42 +93,6 @@ class LogisticRegression:
     
     def print_accuracy(self,Y_pred,Y):
         print("Accuracy: {}%" .format(100 - np.mean(np.abs(Y_pred - Y)) * 100))
-              
-def prepare_image_data(image_dir,test_size = 0.1):
-    
-    images = []
-    label = []
-    classes = {}
-
-    for idx, folder in enumerate(os.listdir(image_dir)):
-        image_path = os.path.join(image_dir,folder)
-        target = folder
-        classes[target] = idx
-        for image in os.listdir(image_path):
-            img = Image.open(os.path.join(image_path, image))
-            resized_img = img.resize((128,128))
-            normalise_img = np.array(resized_img) / 255
-            images.append(normalise_img)
-            
-            label.append(target)
-            
-    images = np.array(images)
-    
-    for i in range(len(label)):
-        label[i] = classes[label[i]]
-        
-    label = np.array(label)
-    
-    x_train, x_test, y_train, y_test = train_test_split(images, label,test_size= test_size, shuffle= True)
-    
-    # Flatten image data
-    x_train = x_train.reshape(x_train.shape[0],-1).T
-    x_test = x_test.reshape(x_test.shape[0],-1).T
-    
-    y_train = y_train.reshape(y_train.shape[0],1).T
-    y_test = y_test.reshape(y_test.shape[0],1).T
-            
-    return x_train, x_test, y_train, y_test
             
 
 if __name__ == '__main__':
